12ed6ae16112d01d65dc050d9acc913c9927397c
mkortib <mkobria@gmail.com>
2019-08-02 12:42:26 +0300

    Updates

---
+++ precache-manifest.74bf746d8b0d5ee777f370392401d5ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2599047817028e98cafee70d5ac92bcc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "85be94f7266fc7e2684d",
+    "url": "/monsters-rolodex/static/css/main.83e106eb.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "85be94f7266fc7e2684d",
+    "url": "/monsters-rolodex/static/js/main.d8de13e6.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
